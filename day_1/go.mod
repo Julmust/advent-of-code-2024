dc92ef99f6811b3de070ea162c60dfd770948cba
Jesper Bergeskans <jesper.bergeskans@gmail.com>
2024-12-01 08:48:25 +0100

    Solution day 1

---
+++ day_1/go.mod
@@ -0,0 +1,3 @@
+module aoc_2024/day_1
+
+go 1.22.3
---
+++ day_1/main.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	ir "aoc_2024/inputreader"
+	"fmt"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func parseData(data []string) ([]int, []int) {
+	var left []int
+	var right []int
+
+	for _, d := range data {
+		rawdata := strings.ReplaceAll(d, "   ", ",")
+		numstrings := strings.Split(rawdata, ",")
+		// fmt.Println(numstrings)
+		leftint, _ := strconv.Atoi(numstrings[0])
+		rightint, _ := strconv.Atoi(numstrings[1])
+
+		left = append(left, leftint)
+		right = append(right, rightint)
+	}
+
+	return left, right
+}
+
+func sortSlice(sl []int) []int {
+	sort.Slice(sl, func(i, j int) bool {
+		return sl[i] < sl[j]
+	})
+
+	return sl
+}
+
+// Writing my own Abs func that takes integers instead of floats :P
+func myAbs(x, y int) int {
+	if x < y {
+		return y - x
+	}
+	return x - y
+}
+
+func two(ldata []int, rdata []int) {
+	var res int
+	for _, i := range ldata {
+		var cnt int
+		for _, j := range rdata {
+			if j == i {
+				cnt++
+			}
+		}
+		res += i * cnt
+	}
+
+	fmt.Printf("Task 2: %v\n", res)
+}
+
+func one(ldata []int, rdata []int) {
+	ldata = sortSlice(ldata)
+	rdata = sortSlice(rdata)
+	var deltas []int
+	for idx := range ldata {
+		deltas = append(deltas, myAbs(ldata[idx], rdata[idx]))
+	}
+
+	var res int
+	for _, v := range deltas {
+		res += v
+	}
+
+	fmt.Printf("Task 1: %v\n", res)
+}
+
+func main() {
+	data := ir.ReadText("input.txt")
+	// data := ir.ReadText("example.txt")
+
+	ldata, rdata := parseData(data)
+	one(ldata, rdata)
+	two(ldata, rdata)
+}
